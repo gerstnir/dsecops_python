6a42bf35b36c9ec6511512c05fe2f335716ca62c
gerstnir <gernir@gmail.com>
2026-05-12 11:43:39 +0300

    refactor operators and string manipulation examples for clarity

--- operators.py
+++ operators.py
@@ -1,26 +1,29 @@
-# A = True
-# B = False
-# C = True
+# boolean operators
 
-# print("A and B: ", A and B)
-# print("C or A: ", C or A)
-# print("not C: ", not C)
-# print("not A: ", not A)
-# print("not B: ", not B)
+A = True
+B = False
+C = True
+
+print("A and B: ", A and B)
+print("C or A: ", C or A)
+print("not C: ", not C)
+print("not A: ", not A)
+print("not B: ", not B)
 
 # boolean expressions
 
-# print(2 > 2 or 2 == 2)
-# print(2 > 2)
-# print(2 <= 2)
+print(2 > 2 or 2 == 2)
+print(2 > 2)
+print(2 <= 2)
 
-# a = 5
-# b = 3
-# c = 2
-# d = 8
-# e = 3
+a = 5
+b = 3
+c = 2
+d = 8
+e = 3
 
-# print ((a>d) or (d>c) or (a>b) and (b==e) and (c!=d))
+# python's interperter runs "and" operators before "or" operators
+print ((a>d) or (d>c) or (a>b) and (b==e) and (c!=d))
 
 
 # float precision
@@ -28,43 +31,49 @@ a = 10
 b = 4.5
 c = 3
 
-# print( "a type: ", type(a), "b type: ", 
-#       type(b), "c type: ", type(c) )
+print( "a type: ", type(a), "b type: ", 
+      type(b), "c type: ", type(c) )
 
-# print( a/b )
-# print( round(a/b, 4) )
+print( a/b )
+print( round(a/b, 4) )
 
-# print( a/c )
-# print( round(a/c, 5) )
+print( a/c )
+print( round(a/c, 5) )
 
-# print( c/b )
-# print( round(a/b, 2) )
+print( c/b )
+print( round(a/b, 2) )
 
-# print( a/b )
-# print(  round(a/b, 0) )
+print( a/b )
+print(  round(a/b, 0) )
 
-# mish = round(9//3, 0)
-# print("mish: ", mish, "type: ", type(mish))
 
-# a = float( input("enter a number: ") ) 
+# showing round vs division to an integer
+mish = round(9/3, 0)
+print("mish: ", mish, "type: ", type(mish))
 
-# # a = float(a)
+mish = 9//3
+print("mish: ", mish, "type: ", type(mish))
 
-# a = int(a)
 
-# print("your number is" + str(a))
 
+# converting decimal number from user input to int can't be done directly
+# and converting to float before converting to int is needed 
+a = float( input("enter a number: ") ) 
+
+a = int(a)
+
+print("your number is", a)
 
 # num = float( input("please enter a number: ") )
 
 # print(num, type(num))
 
+# showing that user input is string by default and no need for casting
+num2_input =  input("please enter another number: ") 
 
-# num2_input =  input("please enter another number: ") 
+# num2 = str(num2_input)
 
-# # num2 = str(num2_input)
-
-# print(num2_input, type(num2_input))
+print(num2_input, type(num2_input))
 
 input_str = input("enter a word: ")
 
--- simple_string.py
+++ simple_string.py
@@ -1,16 +1,19 @@
-# a = "Smart"
-# # b = a[0] + a[1] + "o" + a[3] + a[4]
+a = "Smart"
+b = a[0] + a[1] + "o" + a[3] + a[4]
 
 
-# print(a)
+print("a:", a)
+print("b:", b)
 
-
+# characters and indexes:
+# a	b	c	d	e	f	g	h	i	j	k	l
+# 0	1	2	3	4	5	6	7	8	9	10	11
 
 st = "abcdefghijkl"
 
-s0 = st[1]+st[8]+st[6]
-s1 = st[10]+st[8]+st[3]
-# s2 = 
-# s3 = 
-# s4 = 
-print(s1)
+s0 = st[1] + st[8] + st[6] # big
+s1 = st[10] + st[8] + st[3] # kid
+s2 = st[2] + st[0] + st[10] + st[4] # cake
+s3 = st[1] + st[0] + st[10] + st[4] # bake
+s4 = st[6] + st[8] + st[6] + st[6] + st[11] + st[4] # giggle
+print(s1, s2, s3, s4)
