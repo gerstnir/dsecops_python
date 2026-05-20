de5581d7abdfd4ead2fd0b86e40922151bb9dd09
gerstnir <gernir@gmail.com>
2026-05-20 15:34:06 +0300

    added sol for loop break continue tirgul

--- more_loops/loop_break_continue.py
+++ more_loops/loop_break_continue.py
@@ -13,3 +13,18 @@ for x in range(1,11):
 
 print("loop finished(?)")
 
+# tirgul
+
+for x in range(100,500):
+    if x == 287:
+        print(x)
+        break
+
+print("Done.")
+
+for x in range(10,21):
+    if x == 13:
+        continue
+    print(x)
+
+print("Done #2.")
