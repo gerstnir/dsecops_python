8f9cf9dbfc75aa708688e49ee22041888b670e9d
gerstnir <gernir@gmail.com>
2026-05-20 10:02:55 +0300

    added 20/05 for loop conditions

--- for_loop/for_condition_combo.py
+++ for_loop/for_condition_combo.py
@@ -1,6 +1,44 @@
-for x in range(100):
-    if x % 2 == 0:
-        print(x, "Is Even")
-    else:
-        print(x, "Is Odd")
-    
+# for x in range(100):
+#     if x % 2 == 0:
+#         print(x, "Is Even")
+#     else:
+#         print(x, "Is Odd")
+
+# if a variable is even (זוגי) we get x % 2 == 0 and if it's odd (אי זוגי) we get x % 2 == 1
+# x = 6
+
+# bool_var = x % 2
+# print(bool_var)
+# print(bool(bool_var))
+
+
+# sum_evens = 0
+# sum_odds = 0
+
+# for x in range(1000, 5000):
+#     print(x)
+#     if x % 2 == 0:
+#         sum_evens = sum_evens + x
+#     else:
+#         sum_odds = sum_odds + x
+
+# print("Total sum of evens: ", sum_evens)
+# print("Total sum of odds: ", sum_odds)
+
+# # if x is a vowel print it
+# string = "aejfhbskjhcebewwvdopincxv"
+
+# for x in string:
+#     if x == "a" or x == "e" or x == "i" or x == "o" or x == "u":
+#         print(x)
+
+# # count number of times letter e appears in string
+# string = "aejfhbskjhcebewwvdopincxv"
+
+# count = 0
+
+# for x in string:
+#     if x == "e":
+#         count = count + 1
+
+# print(count)
--- for_loop/for_string.py
+++ for_loop/for_string.py
@@ -1,24 +1,37 @@
 
+# for x in "Smart":
+#     print(x)
+
+# a = "Smart"
+
+# for x in a:
+#     print(x)
 
 # a = input("Type a string: ")
+
+# length = len(a)
+
+# print(length)
 
 # for x in a:
 #     print(x)
 
 # tirgul
 
-# string in loop
-for x in "I love python":
-    print(x)
+# # string in loop
+# for x in "I love python":
+#     print(x)
 
-# string in variable
-a = "smart group"
+# # string in variable
+# a = "smart group"
 
-for x in a:
-    print(x)
+# for x in a:
+#     print(x)
 
 # string in(put)
 a = input("please type a string:\n")
 
 for x in a:
     print(x)
+
+
