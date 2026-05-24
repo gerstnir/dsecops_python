afe5f76fd78fca457093e506f7694c2affd9b1cc
gerstnir <gernir@gmail.com>
2026-05-24 16:37:25 +0300

    refactored function examples in function_adv.py and added comments for clarity

--- functions/function_adv.py
+++ functions/function_adv.py
@@ -12,6 +12,7 @@ func(age=55,city="bat-yam",name="nir")
 
 name = "shlomo"
 
+# default values
 def func2 (name,age=25,city="Tel-aviv"):
     # name =
     # age =
@@ -20,11 +21,17 @@ def func2 (name,age=25,city="Tel-aviv"):
     print("My age is: ", age)
     print("My city is: ", city)
 
-# func("nir", 99, "rehovot")
-# func(age=55,city="bat-yam",name="nir")
-# func2("Moshe", 40,"Ramat gan")
-
-print(name)
-
+func2("Moshe", 40,"Ramat gan")
+# parameters inside the function that have a default values are not required as
+# arguments when calling the function
 func2("aviv")
 
+
+# note that "name" inside the function and outside the function are two different variables
+print(name)
+
+
+
+# tirgul
+
+## see func2 example above^
